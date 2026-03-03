77844afb76262fac1894d1b57acc31592fc9da17
we-BAK <hailemichaelkidist@gmail.com>
2026-03-03 23:17:26 +0300

    RS2

---
+++ Progress_sheet/Reverse_String2.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        j = len(s) - 1
+        for i in range(len(s) // 2):
+            s[i], s[j] = s[j], s[i]
+            j -= 1
