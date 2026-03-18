8103cd146bcf88c2c3768ff592ef3e648569e374
we-BAK <hailemichaelkidist@gmail.com>
2026-03-18 10:37:25 +0300

    chemi_sum

---
+++ Progress_sheet/Chemistry_Sum.py
@@ -0,0 +1,27 @@
+
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+        skill.sort()
+        n = len(skill)
+        
+        total_sum = sum(skill)
+        teams = n // 2
+       
+        if total_sum % teams != 0:
+            return -1
+        
+        target = total_sum // teams
+        
+        left = 0
+        right = n - 1
+        chemistry_sum = 0
+        
+        while left < right:
+            if skill[left] + skill[right] != target:
+                return -1
+            
+            chemistry_sum += skill[left] * skill[right]
+            left += 1
+            right -= 1
+        
+        return chemistry_sum
