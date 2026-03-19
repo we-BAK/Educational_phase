3d7b345e0da58e54ac45129ea63032aa489404d0
we-BAK <hailemichaelkidist@gmail.com>
2026-03-19 10:50:46 +0300

    Runningsum

---
+++ Progress_sheet/Running_Sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        result=[]
+        result.append(nums[0])
+        pre=nums[0]
+        for i in range(1,len(nums)):
+            pre=pre + nums[i]
+            result.append(pre)
+        return(result)
+
+
